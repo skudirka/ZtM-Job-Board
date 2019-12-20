7a243f547e14fe77ee544bf5e63695ebba0de13d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-20 11:54:11 +0000

    Deploy zero-to-mastery/ZtM-Job-Board to github.com/zero-to-mastery/ZtM-Job-Board.git:gh-pages

---
+++ precache-manifest.b4b2e9f908c7f729f12cd44fccfa9fb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b31a369bbe54cbb199c0d0a79e949771",
+    "url": "/ZtM-Job-Board/index.html"
+  },
+  {
+    "revision": "d48171d9734a018d9477",
+    "url": "/ZtM-Job-Board/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "85347abb6acacbfeae92",
+    "url": "/ZtM-Job-Board/static/css/main.944b14ca.chunk.css"
+  },
+  {
+    "revision": "d48171d9734a018d9477",
+    "url": "/ZtM-Job-Board/static/js/2.9f9b46ba.chunk.js"
+  },
+  {
+    "revision": "85347abb6acacbfeae92",
+    "url": "/ZtM-Job-Board/static/js/main.94a109e5.chunk.js"
+  },
+  {
+    "revision": "bf226e520e39ceaa368f",
+    "url": "/ZtM-Job-Board/static/js/runtime-main.b874d388.js"
+  }
+]);
